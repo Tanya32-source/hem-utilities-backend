5285e39c210f8b68b19b826ccbc77b50be6b7a76
Tanya Sinha <sinhat273@gmail.com>
2026-06-29 13:45:28 +0530

    Use Gmail SMTP

--- server.js
+++ server.js
@@ -15,9 +15,11 @@ app.use(express.json());
 // =========================
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
-        user: process.env.EMAIL_USER,          // Your Gmail
+        user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS 
     }
 });
